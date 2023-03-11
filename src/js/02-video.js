21fbdc360cabc3b1490aa968cce424249215b190
MarynaChicherina <marinachicherina.ua@gmail.com>
2023-03-11 14:59:03 +0200

    Add changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+    const iframe = document.querySelector('iframe');
+    const player = Player(iframe);
+
+    const onPlay = function(data) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(data))
+    };
+    const time = player.on('timeupdate', throttle(onPlay, 1000));
+    
+ 
+
+
 
